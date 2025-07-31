d144930c74344f423df875b83da390ea25ce4cc5
ste-bl0 <144320958+sblindow@users.noreply.github.com>
2025-07-31 22:46:42 +0200

    started working on SpatialGraph.h

---
+++ include/SpatialGraph.h
@@ -0,0 +1,28 @@
+#pragma once
+
+#include <vector>
+
+enum class EdgeType {
+  WALKWAY,
+  BUS_LINE,
+  STAIRWAY,
+  STREET
+};
+
+struct Edge {
+  int targetNode;
+  double distance;
+  EdgeType type;
+  int currentLoad = 0; // default member initializer
+};
+
+class SpatialGraph {
+  private:
+    std::vector<std::vector<Edge>> adjacencyList;
+    // location data storage - we'll add this later
+  public:
+    int addNode(); // returns the new node's ID
+    void addEdge(int fromNodeID, int toNodeID, double distance, EdgeType type);
+    std::vector<Edge>& getNeighbors(int NodeID);
+    int getNodeCount();
+};
