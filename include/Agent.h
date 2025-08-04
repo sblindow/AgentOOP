6eb74ad98d32070330ac9f79e966186242fade5b
ste-bl0 <144320958+sblindow@users.noreply.github.com>
2025-08-04 14:28:54 +0200

    implemented constructor of Agent class

--- include/Agent.h
+++ include/Agent.h
@@ -1,11 +1,31 @@
 #pragma once
 
 #include "SpatialGraph.h"
+#include "AgentGraph.h"
 
 #include <string>
 #include <chrono>
 
+enum class Goal {
+  NONE,
+  EAT,
+  SLEEP,
+  BUY_FOOD,
+  WORK  
+};
 
+enum class Activity {
+  IDLE,
+  MOVING,
+  EATING,
+  SLEEPING,
+  SHOPPING,
+  WORKING
+};
+
+
+
+// Agent Class
 class Agent{
 public:
   Agent(SpatialGraph& worldGraph, std::chrono::hours timeOfDay, int iD, std::string name, int home);
@@ -15,9 +35,11 @@ public:
 
   SpatialGraph& worldGraph;
   int worldPosition;
+
+  void tick(); // this is the simulation method that defines what an agent does every tick
   
 private:
-  
+  // core behavior functions
   void evaluateNeeds();
   void percieve();
   void process();
@@ -27,10 +49,30 @@ private:
   void eat();
   void sleep();
 
+  // helper functions
+  void planPathTo(int targetNode);
+  int findNearestNodeType(NodeType targetType);
+  void updateTimers();
+
+  // basic state
   int ticksAwake;
+  int hoursAwake;
   int hunger;
   int energy;
+
+  // Goal and Activity tracking
+  Goal currentGoal;
+  int targetLocation;
+  std::vector<int> pathToTarget;
+  Activity currentActivity;
+  int activityTimer; // countdonw for current Activity
+
+  // property
+  int money;
   int homeLocation;
+  int foodCarrying;
+  int foodFridge;
   
-  SpatialGraph graph; // idiosyncratic knowledge about the world graph
+  // knowledge
+  AgentGraph graph; // Agents idiosyncratic knowledge about the world
 };
--- src/Agent.cpp
+++ src/Agent.cpp
@@ -1,16 +1,45 @@
 #include "Agent.h"
 #include "SpatialGraph.h"
 
+namespace sleepConst {
+  constexpr std::chrono::hours wakeUp {6};
+  constexpr std::chrono::hours sleep {22};
+}
+
+namespace simulationConst {
+  constexpr int hoursPerTick {1};
+  constexpr int energyLoss {2};
+}
+
+// constructor
 Agent::Agent(SpatialGraph& world, std::chrono::hours timeOfDay, int iD, std::string name, int home)
   : agentID {iD}
   , agentName {name}
   , worldGraph {world}
   , worldPosition {home}
-  , ticksAwake {}
-  , hunger {}
-  , energy {}
+  , ticksAwake {0}
+  , hoursAwake {static_cast<int>(
+      (timeOfDay >= sleepConst::wakeUp && timeOfDay < sleepConst::sleep)
+      ? (timeOfDay - sleepConst::wakeUp).count()
+      : std::chrono::hours {0}.count())
+    } // Agents get up at 6AM by default
+  , hunger {20} // start with some hunger
+  , energy {100 - hoursAwake * simulationConst::energyLoss} // energy depends on hours awake and hourly energy loss
+  , currentGoal {Goal::NONE}
+  , targetLocation {-1}
+  , pathToTarget {}
+  , currentActivity {Activity::IDLE}
+  , activityTimer {0}
+  , money {100} // starting balance
+  , homeLocation {home}
+  , foodCarrying {0}
+  , foodFridge {0}
   , graph {}
-{}
+{
+  // initialize Agents perception of starting location
+  WorldNode& startNode = worldGraph.getNode(worldPosition); // get reference to start position world node
+  graph.mapSurroundings(startNode, worldGraph, 3); // map surroundings with initial perception effort of 3
+}
 
 void Agent::evaluateNeeds(){
   
